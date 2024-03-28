658cab5490503e254b9169047d2456b2e8b0ea39
CharisPissouros <cpissouros29@gmail.com>
2024-03-28 13:36:01 +0200

    27/3/2024 changes

--- php/base.php
+++ php/base.php
@@ -9,14 +9,54 @@ $data = json_decode($jsonData, true);
 
 // Access the JSON data
 // Example: Print the value of a specific key
-echo $data['key'];
+//echo $data['key'];
 
-// Rest of your HTML code goes here
+// Connect to the database
+include "get_from_database.php"; //path 
 
+// Iterate through the JSON data and insert into the database
+foreach ($data as $item) {
+    $id = $item['id'];
+    $name = $item['name'];
+    $category = $item['category'];
+
+    // Add more keys as needed
+
+    // Prepare the SQL statement
+    $sql = "INSERT INTO base (product_id, product_name,product_category) VALUES ('$id','$name','$category)";
+    
+
+    // Execute the SQL statement
+    if ($conn->query($sql) === TRUE) {
+        echo "Record inserted successfully";
+    } else {
+        echo "Error inserting record: " . $conn->error;
+    }
+
+    foreach ($data as $item) {
+        $detail_name = $item['detail_name'];
+        $detail_value = $item['detail_value'];
+        $category = $item['category'];
+    
+        
+        // Prepare the SQL statement
+        $sql = "INSERT INTO details (detail_name , detail_value) VALUES ('$detail_name','$detail_value')";
+        
+    
+        
+        // Execute the SQL statement
+        if ($conn->query($sql) === TRUE) {
+            echo "Record inserted successfully";
+        } else {
+            echo "Error inserting record: " . $conn->error;
+        }
+    }
+}
+
+// Close the database connection
+$conn->close();
 ?>
 
 <html>
-
-
-
+    <!-- Your HTML code here -->
 </html>
---
+++ php/get_from_database.php
@@ -0,0 +1,15 @@
+<?php 
+if ($server["REQUEST METHOD" ] == "GET"||"POST"||"PUT"||"DELETE" ){
+
+    $servername = "localhost"; // πρεπει να γινει update με το Localhost της βασης 
+    $username = "root"; //πρεπει να γινει update με το username της βασης 
+    $password = "1234";  //πρεπει να γινει Update με τον κωδικο της βασης
+    $dbname = "database_for_web";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+}
+
+?>
