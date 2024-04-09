f022c5db67f635c2e737ff7a3a038a1000ac66db
Chatzigiannis <127454128+Chatzigiannis@users.noreply.github.com>
2024-04-09 19:19:06 +0300

    Admin can add diasostes !

--- php/admin_main_page.php
+++ php/admin_main_page.php
@@ -3,6 +3,16 @@
     include("get_from_database.php");
     include("functions.php");
     $user = login_sessions($connection);
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $role = 'diasostis';
+        $query = "insert into user (username, password, role) values ('$username', '$password', '$role')";
+        mysqli_query($connection, $query);
+        echo "Neo melos diasosti!";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +29,20 @@
     <h1>Welcome to the main admin page.</h1>
     <br><br>
     Hello, <?php echo $user['username'];?>
+    <br><br>
+    <div id="signup">
+    <h2>Eggrafi neou diasosti</h1>   
+    <form method="POST">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <br>
+        <label for="password">Password</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <br>
+        <input type="submit" id="login" name="Log In">
+    </form>
     <br><br>
     <a href="logout.php">Logout</a>
 </body>
---
+++ php/diasostis_main_page.php
@@ -0,0 +1,25 @@
+<?php
+    session_start();
+    include("get_from_database.php");
+    include("functions.php");
+    $user = login_sessions($connection);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indexnig</title>
+    <style>
+        @import "../css/general_css.css";
+    </style>
+</head>
+<body>
+    <h1>Welcome to the diasostis_main_page.</h1>
+    <br><br>
+    Hello, <?php echo $user['username'];?>
+    <br><br>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
