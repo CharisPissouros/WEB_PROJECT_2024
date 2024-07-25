de7e78b5877e84546b9ce7e5e39a069d80283a25
Chatzigiannis <sotirishad@icloud.com>
2024-07-26 01:05:31 +0300

    Admin can now upload json file from pc or load url

--- php/admin_main_page.php
+++ php/admin_main_page.php
@@ -2,28 +2,36 @@
     session_start();
     include("get_from_database.php");
     include("functions.php");
+    include("json_to_sql.php");
     $user = login_sessions($connection);
 
     if($_SERVER['REQUEST_METHOD'] == "POST")
     {
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        //Check if username exists
-        $query = "select * from user where username = '$username'";
-        $result = mysqli_query($connection, $query);
-        if (mysqli_num_rows($result) > 0)
-        {
-             echo "Username: $username already exists! Try a different one.";
+        if(isset($_POST["username"]) && isset($_POST["password"])){
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            //Check if username exists
+            $query = "select * from user where username = '$username'";
+            $result = mysqli_query($connection, $query);
+            if (mysqli_num_rows($result) > 0)
+            {
+                echo "Username: $username already exists! Try a different one.";
+            }
+            else 
+            {
+                //Username acceptable.
+                $role = 'diasostis';
+                $query = "insert into user (username, password, role) values ('$username', '$password', '$role')";
+                mysqli_query($connection, $query);
+                echo "Neo melos diasosti!";
+            }
+        } elseif (isset($_POST['load_json_from_url'])){
+            url(); 
+        } elseif (isset($_FILES['json_file'])){
+            $file = $_FILES['json_file'];
+            upload($file);
         }
-        else 
-        {
-            //Username acceptable.
-            $role = 'diasostis';
-            $query = "insert into user (username, password, role) values ('$username', '$password', '$role')";
-            mysqli_query($connection, $query);
-            echo "Neo melos diasosti!";
-        }
-        
+
     }
 
 ?>
@@ -36,7 +44,11 @@
     <title>Indexnig</title>
     <style>
         @import "../css/general_css.css";
+        h2 {
+            text-align: center;
+        }
     </style>
+    
 </head>
 <body>
     <h1>Welcome to the main admin page.</h1>
@@ -44,7 +56,8 @@
     Hello, <?php echo $user['username'];?>
     <br><br>
     <div id="signup">
-    <h2>Eggrafi neou diasosti</h1>   
+    <!-- Sign Up Diasosti Form -->
+    <h2>Eggrafi neou diasosti</h2>   
     <form method="POST">
         <label for="username">Username</label>
         <input type="text" id="username" name="username" required>
@@ -56,7 +69,28 @@
         <br>
         <input type="submit" id="login" name="Log In">
     </form>
+    </div>
     <br><br>
+
+    <div id="load_json">
+    <!-- Load JSON From Url -->
+    <h2>Load Json File From Url</h2>
+    <form method="POST">
+        <input type="submit" name="load_json_from_url" value="Load JSON From URL">
+    </form>
+    </div>
+
+    <br><br>
+    <div id="upload_json">
+    <!-- Upload JSON form -->
+    <h2>Upload a JSON File</h2>
+    <form method="POST" enctype="multipart/form-data">
+        <label for="json_file">JSON File</label>
+        <input type="file" id="json_file" name="json_file" required>
+        <br><br>
+        <input type="submit" value="Upload JSON">
+    </form>
+    </div>
     <a href="logout.php">Logout</a>
 </body>
 </html>
--- php/json_to_sql.php
+++ php/json_to_sql.php
@@ -1,51 +1,82 @@
 <?php
-//connection with database
-include ("get_from_database.php"); //path
+include("get_from_database.php");
 
-// Read the JSON file
-$jsonData = file_get_contents('http://usidas.ceid.upatras.gr/web/2023/export.php');
+function url(){
+    // Read the JSON file
+    $jsonData = file_get_contents('http://usidas.ceid.upatras.gr/web/2023/export.php');
 
-// Parse the JSON data
-$data = json_decode($jsonData, true);
+    // Parse the JSON data
+    $data = json_decode($jsonData, true);
 
-// Access the items21P3J21321
-$items = $data['items'];
+    json_to_sql($data);
+}
 
-// Iterate through the JSON data and insert into the database
-foreach ($items as $product) {
-    $id = $product['id'];
-    $name = $product['name'];
-    $category = $product['category'];
+function upload($file){
+    // Read the JSON file
+    //['tmp_name'] = The temporary filename of the file in which the uploaded file was stored on the server.
+    $jsonData = file_get_contents($file['tmp_name']);
 
-    // Prepare the SQL statement
-    $sql_data = "INSERT INTO products (product_id, product_name,product_category) VALUES ('$id','$name','$category')";    
+    // Parse the JSON data
+    $data = json_decode($jsonData, true);
 
-    // Execute the SQL statement
-    if ($connection->query($sql_data) === TRUE) {
-        echo "Record inserted successfully";
-    } else {
-        echo "Error inserting record: " . $connection->error;
-    }
+    json_to_sql($data);
+}
 
+//JSON File From URL
+function json_to_sql($data){
+    global $connection;
+
+    $items = $data['items'];
 
     // Iterate through the JSON data and insert into the database
-    foreach ($product['details'] as $detail) {
-        $detail_name = $detail['detail_name'];
-        $detail_value = $detail['detail_value'];
-   
+    foreach ($items as $product) {
+        $id = $product['id'];
+        $name = $product['name'];
+        $category = $product['category'];
+
         // Prepare the SQL statement
-        $sql_details = "INSERT INTO product_details (product_id , detail_name, detail_value) VALUES ('$id', '$detail_name', '$detail_value')";
-        
-    
-        
+        $sql_data = "INSERT INTO products (product_id, product_name,product_category) VALUES ('$id','$name','$category')";    
+
         // Execute the SQL statement
-        if ($connection->query($sql_details) === TRUE) {
-            echo "Record inserted successfully";
+        if ($connection->query($sql_data) === TRUE) {
+            //echo "Product inserted successfully. <br>";
+            $succesfull_status = true;
         } else {
-            echo "Error inserting record: " . $connection->error;
+            //echo "Error inserting product: " . $connection->error . "<br>";
+            $succesfull_status = false;
+        }
+
+
+        // Iterate through the JSON data and insert into the database
+        foreach ($product['details'] as $detail) {
+            $detail_name = $detail['detail_name'];
+            $detail_value = $detail['detail_value'];
+    
+            // Prepare the SQL statement
+            $sql_details = "INSERT INTO product_details (product_id , detail_name, detail_value) VALUES ('$id', '$detail_name', '$detail_value')";
+            
+        
+            
+            // Execute the SQL statement
+            if ($connection->query($sql_details) === TRUE) {
+                //echo "Detail inserted successfully. <br>";
+                $succesfull_status = true;
+            } else {
+                //echo "Error inserting detail: " . $connection->error . "<br>";
+                $succesfull_status = false;
+            }
         }
     }
+    
+    if ($succesfull_status){
+        $_SESSION['status'] = "Json was succesfully loaded to SQL table";
+        echo "Json was succesfully loaded to SQL table . <br>";
+    }
+    else{
+        $_SESSION['status'] = "Json failed to load to database";
+        echo "Json failed to load to database . <br>";
+    }
+    // Close the database connection
+    $connection->close();
 }
-// Close the database connection
-$connection->close();
 ?>
