4c28e70f9104caf74f9ac17e05af50875351674d
Chatzigiannis <127454128+Chatzigiannis@users.noreply.github.com>
2024-07-25 12:07:08 +0300

    simiosis

--- php/login.php
+++ php/login.php
@@ -1,22 +1,27 @@
 <?php
     session_start();
+    //Ginete to connection me database kai meso tou functions gnorizi pios user ine sindedemenos.
     include("get_from_database.php");
     include("functions.php");
 
-
+    //Me Post pernis ta stoixeia tis formas. Pio asfales apo get afou me get fenontai ta stoixia sto URL.
     if($_SERVER['REQUEST_METHOD'] == "POST")
-    {
+    {   //Dimiourgia php metavliton 
         $username = $_POST['username'];
         $password = $_POST['password'];
+        //epeksigisi sto signup
         $query = "select * from user where username = '$username'";
         $result = mysqli_query($connection, $query);
+        //if result was executed successfull and rows > 1 (i kalo tha itan = 1 efoson den iparxi user me idia stixia)kane login
         if ($result && mysqli_num_rows($result) > 0)
-        {
+        {   //fetch entoli sql i opia metaferi stin metavliti $user ola ta apotelesmata tou query mas. Giafto meta mporoume na xrisimopiisoume px $user['password']
             $user = mysqli_fetch_assoc($result);
+            //elexos an dothike sosto password
             if ($user['password'] === $password)
-            {
+            {   //Thetoume ta stixia sta session
                 $_SESSION['user_id'] = $user['user_id'];
                 $_SESSION['role'] = $user['role'];
+                //Elegxoume to role gia na kseroume se pio main page na katefthinthoume.
                 if ($_SESSION['role'] === "admin")
                 {
                     header("Location: admin_main_page.php");
@@ -36,7 +41,7 @@
         }
         else
         {
-            echo "Wrong Information!";
+            echo "Wrong Information! Try again.";
         }
         
     }
--- php/signup.php
+++ php/signup.php
@@ -3,14 +3,16 @@
     include("get_from_database.php");
     include("functions.php");
 
-
     if($_SERVER['REQUEST_METHOD'] == "POST")
     {
         $username = $_POST['username'];
-        $password = $_POST['password'];
-        //Check if username exists
+        $password = $_POST['password']; 
+        //Vazoume to sql line pou theloume se metavliti gia na tin kalesoume sto mysqli_query oste na ektelesti
         $query = "select * from user where username = '$username'";
+        //mysqli_query vazoume to $connection pou antistixi sto server sql p apefthinomaste, kai me to $query stelnoume tin entoli sql pros ektelesi.
+        //Tote to result exi ta apotelesmata tou query mas apo to database pou antistixi sto $connection (vlepe get_from_database gia connection).
         $result = mysqli_query($connection, $query);
+        //an exoume 1+ grammes (apandisis) simeni oti iparxi o xristis ara den mporis na xrisimopoiisis ta idia stoixeia
         if (mysqli_num_rows($result) > 0){
             echo "Username: $username already exists! Try a different one.";
         }
@@ -18,8 +20,11 @@
         {
             //Username is acceptable.
             $role = "politis";
+            //$query antistoixi se entoli sql pros ektelesi.
             $query = "insert into user (username, password, role) values ('$username', '$password', '$role')";
+            //function gia enosi database kai ektelesi query
             mysqli_query($connection, $query);
+            //meta tin ektelesi anakatefthinsi se login page.
             header("Location: login.php");
             exit;
         }
