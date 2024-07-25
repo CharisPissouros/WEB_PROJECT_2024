efc9cbbc9abe0b60a1f8fa56340501eb342c8de8
Chatzigiannis <sotirishad@icloud.com>
2024-07-25 19:15:17 +0300

    web_project2024

--- php/get_from_database.php
+++ php/get_from_database.php
@@ -2,7 +2,7 @@
     $servername = "localhost"; // πρεπει να γινει update με το Localhost της βασης 
     $dbusername = "root"; //πρεπει να γινει update με το username της βασης 
     $dbpassword = "1234";  //πρεπει να γινει Update με τον κωδικο της βασης
-    $dbname = "web_project";
+    $dbname = "web_project2024";
 
     $connection = new mysqli($servername, $dbusername, $dbpassword, $dbname);
     if ($connection->connect_error) {
